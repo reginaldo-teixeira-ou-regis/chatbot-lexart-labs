bfbb7e67bb16dbe17d0e0a503750ef84a57bd1c2
Reginaldo Teixeira <reginaldoteixeiraouregis@gmail.com>
2023-07-19 12:39:57 -0300

    Update

--- src/components/pages/Interaction/index.tsx
+++ src/components/pages/Interaction/index.tsx
@@ -12,8 +12,8 @@ export default function Interaction() {
     <main className="w-full flex flex-col items-center">
       <div className="bg-slate-500 md:max-w-[50%] w-full min-h-screen flex flex-col items-center justify-between">
         <section id="chatContainer" className="w-full h-full max-h-[calc(100vh-3.5rem)] overflow-y-auto p-3 scroll-d">
-          {messages.map((message, index) => (
-            <div key={index} className={twMerge('bg-slate-600 flex flex-col rounded-md p-3 justify-start gap-2 w-full mb-3', message.sentBy === 'system' && 'bg-slate-700')}>
+          {messages.map((message) => (
+            <div key={message.userId} className={twMerge('bg-slate-600 flex flex-col rounded-md p-3 justify-start gap-2 w-full mb-3', message.sentBy === 'system' && 'bg-slate-700')}>
               <p className="text-white break-words" dangerouslySetInnerHTML={{ __html: message.message}}/>
               {
                 message.options && (
